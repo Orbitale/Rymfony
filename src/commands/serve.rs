f09153b575e60e23ba15ec7214fc8926df63a8d5
Alex Rock Ancelet <pierstoval@gmail.com>
2020-06-18 00:32:26 +0200

    Make web server functional by using Hyper and Tokio

--- src/commands/serve.rs
+++ src/commands/serve.rs
@@ -1,7 +1,38 @@
-use clap::App;
-use clap::Arg;
 use clap::ArgMatches;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::net::ToSocketAddrs;
+use hyper::Response;
+use hyper::Request;
+use hyper::Body;
+use hyper::Server;
+use hyper::service::make_service_fn;
+use hyper::service::service_fn;
 
-pub fn serve(arguments: ArgMatches) {
-    println!("Serving HTTP stuff...");
+#[tokio::main]
+pub async fn serve(listen: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    pretty_env_logger::init();
+
+    let mut sockets = listen.to_socket_addrs().unwrap();
+
+    let addr = SocketAddr::from(sockets.next().unwrap());
+
+    let service_handler = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(request_handler))
+    });
+
+    println!("Serving on http://{}", addr);
+
+    let server = Server::bind(&addr).serve(service_handler);
+
+    server.await?;
+
+    Ok(())
+}
+
+async fn request_handler(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!("{} {}", _req.method(), _req.uri());
+
+    Ok(Response::new("Hello, World".into()))
 }
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ use clap::Arg;
 
 use crate::commands::serve::serve;
 
+const DEFAULT_LISTEN: &str = "127.0.0.1:5000";
+
 fn main() {
-    let app = App::new("rustphp")
+    let app = App::new("rymfony")
         .version("0.1")
         .author("Alex Rock <alex@orbitale.io>")
         .about("To be determined")
@@ -16,24 +18,34 @@ fn main() {
             App::new("serve")
                 .about("Runs an HTTP server")
                 .arg(
-                    Arg::with_name("port")
-                        .short("p")
-                        .long("port")
-                        .help("The HTTP port to listen to")
+                    Arg::with_name("listen")
+                        .short("l")
+                        .long("listen")
+                        .default_value(DEFAULT_LISTEN)
+                        .help("The TCP socket to listen to, usually an IP with a Port")
                 )
         ])
     ;
 
     let matches = app.get_matches();
 
+    let listen = match matches.value_of("listen") {
+        Some(t) => {
+            println!("Something something {}", t);
+            t
+        }
+        None => DEFAULT_LISTEN
+    };
+
     match matches.subcommand_name() {
         Some("serve")  => {
             println!("Executed command: {}", matches.subcommand_name().unwrap());
-            serve(matches);
+
+            serve(listen);
         },
         _ => {
             println!("Executed no command. Execute \"serve\" by default");
-            serve(matches);
+            serve(listen);
         },
     }
     ;
