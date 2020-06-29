640b7b077743692c06987760fcb0f8b9e582089e
Alex Rock Ancelet <pierstoval@gmail.com>
2020-06-30 00:12:51 +0200

    Separate different PHP server types into different files & modules

--- src/utils/php_server.rs
+++ src/utils/php_server.rs
@@ -1,24 +1,33 @@
 use crate::utils::php_binaries;
-use crate::utils::php_server_fpm::start_fpm;
+use crate::utils::php_server_cgi::start as start_cgi;
+use crate::utils::php_server_fpm::start as start_fpm;
+use crate::utils::php_server_native::start as start_native;
 
-// Check: https://www.php.net/manual/en/install.fpm.configuration.php
+use std::thread;
+use std::time;
 
 pub(crate) fn start() {
     let php_bin = php_binaries::current();
 
-    if php_bin.contains("-fpm") && cfg!(not(target_family = "windows")) {
-        start_fpm(php_bin);
+    let mut process = if php_bin.contains("-fpm") && cfg!(not(target_family = "windows")) {
+        start_fpm(php_bin)
     } else if php_bin.contains("-cgi") {
-        start_cgi(php_bin);
+        start_cgi(php_bin)
     } else {
-        start_native(php_bin);
+        start_native(php_bin)
+    };
+
+    let sleep_time = time::Duration::from_millis(1000);
+    thread::sleep(sleep_time);
+
+    match process.try_wait() {
+        Ok(Some(status)) => panic!(format!("PHP server exited with {}", status)),
+        Ok(None) => {
+            println!("PHP server seems ready");
+        }
+        Err(e) => panic!(format!(
+            "An error occured when checking PHP server health: {:?}",
+            e
+        )),
     }
-}
-
-fn start_cgi(php_bin: String) {
-    println!("Todo: serve php-cgi");
-}
-
-fn start_native(php_bin: String) {
-    println!("Todo: serve php native");
 }
