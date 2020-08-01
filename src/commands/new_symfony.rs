64360d3e332b622bca87c4d38b4c5bb2596601f7
Alex Rock Ancelet <pierstoval@gmail.com>
2020-08-01 12:45:16 +0200

    Finish the "new" command with args "full" and "no-git"

--- src/commands/new_symfony.rs
+++ src/commands/new_symfony.rs
@@ -13,14 +13,27 @@ pub(crate) fn command_config<'a, 'b>() -> App<'a, 'b> {
         .alias("new")
         .about("Create a new Symfony project")
         .arg(
-            Arg::with_name("dit")
+            Arg::with_name("directory")
                 .index(1)
                 .help("The directory in which the project will be created"),
+        )
+        .arg(
+            Arg::with_name("full")
+                .long("full")
+                .help("Use the symfony/website-skeleton instead of the default one")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("no-git")
+                .long("no-git")
+                .help("Do not initialize the project with git"),
         )
 }
 
 pub(crate) fn new_symfony(args: &ArgMatches) {
-    let mut directory = args.value_of("dit").unwrap_or("").to_string();
+    let mut directory = args.value_of("directory").unwrap_or("").to_string();
+    let full = args.is_present("full");
+    let initialize_git = !args.is_present("no_git");
 
     if directory == "" {
         let path = env::current_dir().unwrap()
@@ -49,24 +62,47 @@ pub(crate) fn new_symfony(args: &ArgMatches) {
     info!("Composer detected at {}", &composer_path.to_str().unwrap());
 
     let mut command = Command::new(composer_path.to_str().unwrap());
-    let package = String::from("symfony/skeleton");
 
     command
         .stdin(Stdio::null())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .arg("create-project")
-        .arg(&package)
+        .arg(if full {
+            "symfony/website-skeleton"
+        } else {
+            "symfony/skeleton"
+        })
         .arg(path.to_str().unwrap())
     ;
 
     match command.output() {
-        Ok(_) => {
-            info!("Done!");
-        },
+        Ok(_) => (),
         Err(e) => {
             error!("Could not create project");
             error!("{}", e);
         }
     };
+
+    if initialize_git {
+        let git_path = which::which("git").unwrap();
+
+        let mut command = Command::new(git_path.to_str().unwrap());
+
+        command
+            .stdin(Stdio::null())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .arg("init")
+            .arg(path.to_str().unwrap())
+        ;
+
+        match command.output() {
+            Ok(_) => (),
+            Err(e) => {
+                error!("Could not initialize git");
+                error!("{}", e);
+            }
+        };
+    }
 }
