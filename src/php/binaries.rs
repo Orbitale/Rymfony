d3d239ccf1d5edc040efea4d65f74ad28aa4e772
Alex Rock Ancelet <pierstoval@gmail.com>
2020-08-05 22:19:58 +0200

    More rework of how php binaries are found

--- src/php/binaries.rs
+++ src/php/binaries.rs
@@ -1,85 +1,45 @@
-use glob::glob;
-use rayon::prelude::*;
-use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 use std::process::Stdio;
 use std::str;
+
+use glob::glob;
+use rayon::prelude::*;
+use regex::Regex;
+
+use crate::php::structs::PhpBinary;
 use crate::php::structs::PhpServerSapi;
 use crate::php::structs::PhpVersion;
-use crate::php::structs::PhpBinary;
-use std::collections::HashMap;
+use tokio::stream::StreamExt;
 
 pub(crate) fn current() -> String {
     let binaries = all();
 
-    let binaries_regex = if cfg!(target_family = "windows") {
-        // On Windows, we mostly have "php" and "php-cgi"
-        Regex::new(r"php(\d+(\.\d+))?(-cgi)\.exe$").unwrap()
-    } else {
-        // This will probably need to be updated for other platforms.
-        // This matches "php", "php7.4", "php-fpm", "php7.4-fpm" and "php-fpm7.4"
-        Regex::new(r"php(\d+(\.\d+))?(-fpm)(\d+(\.\d+))?$").unwrap()
-    };
-
-    for binary in binaries {
-        if !binaries_regex.is_match(binary.as_str()) {
-            continue;
-        }
-
+    for (_version, binary) in binaries {
         // TODO: check for a better solution to choose current PHP version
-        return binary.to_string();
+        return binary.path().to_string();
     }
 
     "php".to_string()
 }
 
 pub(crate) fn all() -> HashMap<PhpVersion, PhpBinary> {
+    let binaries: HashMap<PhpVersion, PhpBinary> = HashMap::new();
 
-    let mut binaries: Vec<String> = Vec::new();
+    let mut binaries = binaries_from_env(binaries);
 
-    if cfg!(target_family = "windows") {
-        binaries.extend(binaries_from_env());
-    } else {
-        binaries.extend(binaries_from_path(PathBuf::from("/usr/bin")))
-    };
+    merge_binaries(
+        binaries_from_dir(PathBuf::from("/usr/bin")),
+        &binaries
+    );
 
-    let php_version_output_regex = Regex::new(r"^PHP (\d\.\d+\.\d+) \(([^\)])+\)").unwrap();
-
-    let binaries: Vec<PhpBinary> = binaries
-        .into_par_iter()
-        .filter(|binary| {
-            let version = get_version_from_binary(&binary);
-            php_version_output_regex.is_match(version.trim())
-        })
-        .map(|binary| {
-            let version = get_version_from_binary(&binary);
-
-            let capts = php_version_output_regex.captures(&binary).unwrap();
-
-            let sapi = match &capts[2] {
-                #[cfg(not(target_os = "windows"))]
-                "FPM" => PhpServerSapi::FPM,
-                "CLI" => PhpServerSapi::CLI,
-                "CGI" => PhpServerSapi::CGI,
-                _ => PhpServerSapi::Unknown,
-            };
-
-            PhpBinary::from(version.trim().to_string(), PathBuf::from(binary), sapi)
-        })
-        .collect();
-
-    let mut versions: HashMap<PhpVersion, PhpBinary> = HashMap::new();
-
-    for bin in binaries {
-        let php_version = PhpVersion::from_version(&String::from(""));
-    }
-
-    versions
+    binaries
 }
 
-fn binaries_from_env() -> Vec<String> {
+fn binaries_from_env(binaries: HashMap<PhpVersion, PhpBinary>) -> HashMap<PhpVersion, PhpBinary> {
     let path_string = env::var_os("PATH").unwrap();
     let path_dirs = path_string
         .to_str()
@@ -91,22 +51,30 @@ fn binaries_from_env() -> Vec<String> {
         })
         .collect::<Vec<&str>>();
 
+    let mut binaries: HashMap<PhpVersion, PhpBinary> = HashMap::new();
+
     path_dirs
         .into_par_iter()
         .map(|dir| {
-            binaries_from_path(PathBuf::from(dir))
-        })
-        .flatten()
-        .collect()
+            let binaries_from_dir = binaries_from_dir(PathBuf::from(dir));
+
+            for (version, binary) in binaries_from_dir {
+                let mut current: PhpBinary = if binaries.contains_key(&version) {
+                    binaries[&version]
+                } else {
+                    PhpBinary::from_version(version.clone())
+                };
+
+                current.merge_with(binary);
+            }
+        });
+
+    binaries
 }
 
-fn binaries_from_path(path: PathBuf) -> Vec<String> {
-    let mut binaries: Vec<String> = vec![];
-
+fn binaries_from_dir(path: PathBuf) -> HashMap<PhpVersion, PhpBinary> {
     if !path.is_dir() {
-        println!("not a dir {}", path.to_str().unwrap());
-
-        return binaries;
+        return HashMap::new();
     }
 
     let binaries_regex = if cfg!(target_family = "windows") {
@@ -115,8 +83,10 @@ fn binaries_from_path(path: PathBuf) -> Vec<String> {
     } else {
         // This will probably need to be updated for other platforms.
         // This matches "php", "php7.4", "php-fpm", "php7.4-fpm" and "php-fpm7.4"
-        Regex::new(r"php(\d+(\.\d+))?(-fpm)?(\d+(\.\d+))?$").unwrap()
+        Regex::new(r"php(\d+(\.\d+))?([_-]?fpm)?(\d+(\.\d+))?$").unwrap()
     };
+
+    let mut binaries_paths: Vec<String> = Vec::new();
 
     let path_glob = glob_from_path(path.display().to_string().as_str());
 
@@ -125,8 +95,29 @@ fn binaries_from_path(path: PathBuf) -> Vec<String> {
         if !binaries_regex.is_match(binary.to_str().unwrap()) {
             continue;
         }
+        let binary = binary.canonicalize().unwrap();
 
-        binaries.push(binary.to_str().unwrap().parse().unwrap());
+        binaries_paths.push(binary.to_str().unwrap().parse().unwrap());
+    }
+
+    let binaries_paths: HashSet<String> = binaries_paths.iter().cloned().collect();
+
+    let mut binaries: HashMap<PhpVersion, PhpBinary> = HashMap::new();
+
+    for path in binaries_paths {
+        let (version, sapi) = get_binary_metadata(&path);
+
+        if binaries.contains_key(&version) {
+            let mut current = binaries[&version];
+
+            if !current.has_sapi(&sapi) {
+                current.add_sapi(&sapi, &path);
+            }
+        } else {
+            let mut bin = PhpBinary::from_version(&version);
+            bin.add_sapi(&sapi, &path);
+            &binaries.insert(version.clone(), bin);
+        }
     }
 
     binaries
@@ -140,14 +131,45 @@ fn glob_from_path(path: &str) -> String {
     }
 }
 
-fn get_version_from_binary(binary: &str) -> String {
-    let process = Command::new(binary.as_str())
+fn get_binary_metadata(binary: &str) -> (PhpVersion, PhpServerSapi) {
+    let process = Command::new(binary)
         .arg("--version")
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
 
     let output = process.wait_with_output().unwrap();
+    let output = String::from(output.stdout.to_str().unwrap());
 
-    String::from_utf8(output.stdout).unwrap()
+    let php_version_output_regex = Regex::new(r"^PHP (\d\.\d+\.\d+) \(([^\)]+)\)").unwrap();
+
+    if !php_version_output_regex.is_match(&output) {
+        panic!(
+            "Version \"{}\" for php binary \"{}\" is invalid.",
+            &output, &binary
+        );
+    }
+
+    let capts = php_version_output_regex.captures(&output).unwrap();
+    let version = &capts[1];
+    let sapi = &capts[2];
+
+    (
+        PhpVersion::from(version.to_string()),
+        PhpServerSapi::from_str(&sapi),
+    )
+}
+
+fn merge_binaries(
+    from: HashMap<PhpVersion, PhpBinary>,
+    mut into: &HashMap<PhpVersion, PhpBinary>
+) {
+    for (version, binary) in from {
+        let mut current_into = if into.contains_key(&version) {
+            into[version]
+        } else {
+            PhpBinary::from_version(version.clone())
+        };
+        current_into.merge_with(binary);
+    }
 }
--- src/php/structs.rs
+++ src/php/structs.rs
@@ -1,21 +1,55 @@
-use std::path::PathBuf;
+use regex::Regex;
 
 pub(crate) enum PhpServerSapi {
-    #[cfg(not(target_os = "windows"))]
     FPM,
     CGI,
     CLI,
-    Unknown
+    Unknown,
 }
 
+impl PhpServerSapi {
+    pub(crate) fn from_str(str: &str) -> PhpServerSapi {
+        match str {
+            "FPM" => PhpServerSapi::FPM,
+            "CLI" => PhpServerSapi::CLI,
+            "CGI" => PhpServerSapi::CGI,
+            _ => PhpServerSapi::Unknown,
+        }
+    }
+
+    pub(crate) fn all() -> Vec<PhpServerSapi> {
+        vec![
+            PhpServerSapi::FPM,
+            PhpServerSapi::CLI,
+            PhpServerSapi::CGI,
+        ]
+    }
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+
+#[derive(Hash, Eq, PartialEq)]
 pub(crate) struct PhpVersion {
-    _version: String
+    _version: String,
 }
 
 impl PhpVersion {
-    pub(crate) fn from_version(version: &String) -> PhpVersion {
+    pub(crate) fn from(version: String) -> PhpVersion {
+        let version_regex = Regex::new("^[578]\\.\\d{1,2}\\.\\d+$").unwrap();
+
+        if !version_regex.is_match(&version) {
+            panic!("Version \"{}\" is not a valid PHP version.", &version);
+        }
+
         PhpVersion {
-            _version: version.clone()
+            _version: version.clone(),
         }
     }
 
@@ -24,19 +58,55 @@ impl PhpVersion {
     }
 }
 
+//
+//
+//
+//
+//
+//
+//
+//
+
+#[derive(Hash, Eq, PartialEq)]
 pub(crate) struct PhpBinary {
-    _version: String,
-    _path: PathBuf,
-    _sapi: PhpServerSapi
+    _version: PhpVersion,
+    directory: String,
+    cli: String,
+    fpm: String,
+    cgi: String,
+    default: bool,
 }
 
 impl PhpBinary {
-    pub(crate) fn from(version: String, path: PathBuf, sapi: PhpServerSapi) -> PhpBinary {
+    pub(crate) fn from_version(version: PhpVersion) -> PhpBinary {
         PhpBinary {
             _version: version,
-            _path: path,
-            _sapi: sapi
+            directory: String::from(""),
+            cli: String::from(""),
+            fpm: String::from(""),
+            cgi: String::from(""),
+            default: false,
         }
+    }
+
+    pub(crate) fn set_directory(&mut self, directory: String) {
+        self.directory = directory.clone();
+    }
+
+    pub(crate) fn merge_with(&mut self, from: PhpBinary) {
+        if self._version != from._version {
+            return;
+        }
+
+        for sapi in PhpServerSapi::all() {
+            if from.has_sapi(&sapi) && !self.has_sapi(&sapi) {
+                self.add_sapi(&sapi, &from.sapi_path(&sapi));
+            }
+        }
+    }
+
+    pub(crate) fn set_default(&mut self, default: bool) {
+        self.default = default;
     }
 
     pub(crate) fn version(&self) -> &str {
@@ -44,6 +114,39 @@ impl PhpBinary {
     }
 
     pub(crate) fn path(&self) -> &str {
-        self._path.as_str()
+        self._path.to_str().unwrap()
+    }
+
+    pub(crate) fn has_sapi(&self, sapi: &PhpServerSapi) -> bool {
+        match sapi {
+            PhpServerSapi::FPM => self.fpm != "",
+            PhpServerSapi::CLI => self.cli != "",
+            PhpServerSapi::CGI => self.cgi != "",
+            PhpServerSapi::Unknown => false,
+        }
+    }
+
+    pub(crate) fn add_sapi(&mut self, sapi: &PhpServerSapi, path: &String) {
+        match sapi {
+            PhpServerSapi::FPM => {
+                self.fpm = path.clone();
+            }
+            PhpServerSapi::CLI => {
+                self.cli = path.clone();
+            }
+            PhpServerSapi::CGI => {
+                self.cgi = path.clone();
+            }
+            PhpServerSapi::Unknown => false,
+        }
+    }
+
+    pub(crate) fn sapi_path(&self, sapi: &PhpServerSapi) -> String {
+        match sapi {
+            PhpServerSapi::FPM => self.fpm.clone(),
+            PhpServerSapi::CLI => self.cli.clone(),
+            PhpServerSapi::CGI => self.cgi.clone(),
+            PhpServerSapi::Unknown => false,
+        }
     }
 }
