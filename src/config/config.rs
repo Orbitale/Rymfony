2c05e6de1f6ca91e2fa142961edc9f0b8682af14
Alex Rock Ancelet <pierstoval@gmail.com>
2020-08-27 15:27:50 +0200

    Saving PHP versions to file functional in WSL!

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,9 +1,17 @@
 use std::collections::HashMap;
 use crate::php::structs::PhpVersion;
 use crate::php::structs::PhpBinary;
+use dirs::home_dir;
+use std::fs::File;
+use std::io::Write;
 
 pub(crate) fn save_binaries_to_config(binaries: &HashMap<PhpVersion, PhpBinary>) {
     let serialized = serde_json::to_string_pretty(&binaries).unwrap();
 
-    //TODO: write file
+    let versions_file_path = home_dir().unwrap().join(".rymfony").join("php-versions.json");
+
+    let mut versions_file = File::create(versions_file_path).unwrap();
+
+    versions_file.write_all(serialized.as_bytes())
+        .expect("Could not write PHP versions to JSON file.");
 }
